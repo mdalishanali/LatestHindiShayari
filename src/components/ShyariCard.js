cd850e41ddd5b5b1a3766c9951c1b6df226091c7
MD ALISHAN ALI <md.alishanali88@gmail.com>
2022-12-17 13:52:10 +0530

    shayari added

---
+++ src/components/ShyariCard.js
@@ -0,0 +1,89 @@
+import {
+  Linking,
+  Share,
+  StyleSheet,
+  Text,
+  ToastAndroid,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import React from "react";
+import { COLORS } from "../constants";
+import { FontAwesome, FontAwesome5 } from "@expo/vector-icons";
+import * as Clipboard from "expo-clipboard";
+
+const ShyariCard = ({ title }) => {
+  const copyToClipboard = async () => {
+    await Clipboard.setStringAsync("hello world");
+    ToastAndroid.show("Copied!", ToastAndroid.BOTTOM);
+  };
+
+  const socialShare = async () => {
+    await Share.share({
+      title: "Shayari",
+      message: "quote",
+    });
+  };
+
+  const tweetNow = () => {
+    const url = "https://twitter.com/intent/tweet?text=" + "quote";
+    Linking.openURL(url);
+  };
+  return (
+    <View style={styles.main}>
+      <View>
+        <Text style={styles.shayariText}>{title}</Text>
+      </View>
+      <View style={styles.btnParent}>
+        <View style={styles.btnCont}>
+          <TouchableOpacity onPress={socialShare}>
+            <FontAwesome name="share-square-o" style={styles.btnIcon} />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={copyToClipboard}>
+            <FontAwesome5 name="copy" style={styles.btnIcon} />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={tweetNow}>
+            <FontAwesome name="twitter" style={styles.btnIcon} />
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export default ShyariCard;
+
+const styles = StyleSheet.create({
+  main: {
+    backgroundColor: COLORS.secondery,
+    margin: 10,
+    borderRadius: 20,
+    borderColor: "#6A6767",
+    borderWidth: 1,
+  },
+  shayariText: {
+    padding: 20,
+    color: COLORS.white,
+    fontSize: 25,
+    textAlign: "center",
+  },
+  btnParent: {
+    borderColor: COLORS.white,
+    borderTopWidth: 1,
+    borderStyle: "dotted",
+    backgroundColor: "#1F232D",
+    borderBottomLeftRadius: 20,
+    borderBottomRightRadius: 20,
+  },
+  btnCont: {
+    display: "flex",
+    justifyContent: "space-around",
+    flexDirection: "row",
+    padding: 5,
+  },
+  btnIcon: {
+    color: COLORS.white,
+    fontSize: 30,
+    padding: 10,
+  },
+});
--- src/screens/ListShayari.js
+++ src/screens/ListShayari.js
@@ -1,22 +1,44 @@
-import { StyleSheet, Text, View } from "react-native";
-import React, { useState } from "react";
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+  VirtualizedList,
+} from "react-native";
+import React, { useEffect, useState } from "react";
 import { useRoute } from "@react-navigation/native";
+import ShyariCard from "../components/ShyariCard";
+import { loveShayari } from "../shyariDB/love";
+import { DostiShayari } from "../shyariDB/dost";
 
 const ListShayari = () => {
   const route = useRoute();
   const category = route.params.category;
   const [data, setData] = useState([]);
-  switch (category) {
-    case "value":
-      break;
+  useEffect(() => {
+    switch (category) {
+      case "Love Shayari":
+        setData(loveShayari);
+        break;
+      case "Dosti Shayari":
+        setData(DostiShayari);
+        break;
+      default:
+        break;
+    }
+  }, []);
 
-    default:
-      break;
-  }
+  const getItem = (data, index) => data[index];
+
   return (
-    <View>
-      <Text>ListShayari:{data}</Text>
-    </View>
+    <VirtualizedList
+      data={data}
+      initialNumToRender={4}
+      renderItem={({ item }) => <ShyariCard title={item.title} key={item.id} />}
+      keyExtractor={(item, index) => index}
+      getItemCount={(data) => data.length}
+      getItem={getItem}
+    />
   );
 };
 
